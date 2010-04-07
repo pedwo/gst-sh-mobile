04cb6fa62df630a0753203c224598bd77294b185
Phil Edworthy <phil.edworthy@renesas.com>
2010-04-07 17:12:34 +0900

    autotools: Major clean up of how autotools are used
    This fixes the libtool issue with gst-sh-mobile. This is a wholesale
    cleanup of the autotools stuff, largely based on TI’s plugins.
    
    Also simplified the checks for uiomux, libshveu & libshcodecs as it
    made the changes simpler and I think that we can rely on pkg_config and
    the correct versions now.

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-echo "Currently this autogen.sh should be run outside the scratchbox."
+DIE=0
+package=gst-shvideo
 
-echo source helper functions
-if test ! -f common/gst-autogen.sh;
+# autogen.sh helper functions (copied from GStreamer's common/ CVS module)
+if test ! -f ./gst-autogen.sh;
 then
   echo There is something wrong with your source tree.
-  echo You are missing common/gst-autogen.sh
+  echo You are either missing ./gst-autogen.sh or not
+  echo running autogen.sh from the top-level source
+  echo directory.
   exit 1
 fi
-. common/gst-autogen.sh
+. ./gst-autogen.sh
 
 CONFIGURE_DEF_OPT='--enable-maintainer-mode'
 
@@ -18,12 +21,14 @@ autogen_options $@
 
 echo -n "+ check for build tools"
 if test ! -z "$NOCHECK"; then echo " skipped"; else  echo; fi
-version_check "autoconf" "$AUTOCONF autoconf autoconf-2.54 autoconf-2.53 autoconf-2.52" \
+version_check "autoconf" "$AUTOCONF autoconf autoconf259 autoconf257 autoconf-2.54 autoconf-2.53 autoconf-2.52" \
               "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || DIE=1
-version_check "automake" "$AUTOMAKE automake automake-1.9 automake-1.7 automake-1.6 automake-1.5" \
+version_check "automake" "$AUTOMAKE automake automake-1.9 automake19 automake-1.7 automake-1.6 automake-1.5" \
               "ftp://ftp.gnu.org/pub/gnu/automake/" 1 7 || DIE=1
-version_check "autopoint" "autopoint" \
-              "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 14 || DIE=1
+LTVER="unknown"
+version_check "libtoolize" "$LIBTOOLIZE libtoolize glibtoolize" \
+              "ftp://ftp.gnu.org/pub/gnu/libtool/" 2 2 0 > /dev/null \
+              && LTVER=2_2
 version_check "libtoolize" "$LIBTOOLIZE libtoolize glibtoolize" \
               "ftp://ftp.gnu.org/pub/gnu/libtool/" 1 5 0 || DIE=1
 version_check "pkg-config" "" \
@@ -40,37 +45,23 @@ die_check $DIE
 # if no arguments specified then this will be printed
 if test -z "$*"; then
   echo "+ checking for autogen.sh options"
-#  echo "  This autogen script will NOT automatically run ./configure"
-#  echo "  This autogen script will automatically run ./configure as:"
 #  echo "  This autogen script will automatically run ./configure as:"
 #  echo "  ./configure $CONFIGURE_DEF_OPT"
 #  echo "  To pass any additional options, please specify them on the $0"
 #  echo "  command line."
 fi
 
-toplevel_check $srcfile
-toplevel_check $srcfile2
-
-# autopoint
-#    older autopoint (< 0.12) has a tendency to complain about mkinstalldirs
-if test -x mkinstalldirs; then rm mkinstalldirs; fi
-#    first remove patch if necessary, then run autopoint, then reapply
-if test -f po/Makefile.in.in;
+if [ ${LTVER} = "2_2" ]
 then
-  patch -p0 -R < common/gettext.patch
+    tool_run "$libtoolize" "--copy --force"
+    tool_run "$aclocal" "-I m4/ $ACLOCAL_FLAGS"
+else
+    tool_run "$aclocal" "-I m4/ $ACLOCAL_FLAGS"
+    tool_run "$libtoolize" "--copy --force"
 fi
-#tool_run "$autopoint --force"
-#patch -p0 < common/gettext.patch
-
-tool_run "$aclocal" "-I m4 -I common/m4 $ACLOCAL_FLAGS"
-tool_run "$libtoolize" "--copy --force"
 tool_run "$autoheader"
-
-# touch the stamp-h.in build stamp so we don't re-run autoheader in maintainer mode -- wingo
-echo timestamp > stamp-h.in 2> /dev/null
-
 tool_run "$autoconf"
-tool_run "$automake" "-a -c -Wno-portability"
+tool_run "$automake" "-a -c"
 
 # if enable exists, add an -enable option for each of the lines in that file
 if test -f enable; then
@@ -86,4 +77,21 @@ if test -f disable; then
   done
 fi
 
-echo "Now type run ./configure inside scratchbox."
+test -n "$NOCONFIGURE" && {
+  echo "+ skipping configure stage for package $package, as requested."
+  echo "+ autogen.sh done."
+  exit 0
+}
+
+echo "+ running configure ... "
+test ! -z "$CONFIGURE_DEF_OPT" && echo "  ./configure default flags: $CONFIGURE_DEF_OPT"
+test ! -z "$CONFIGURE_EXT_OPT" && echo "  ./configure external flags: $CONFIGURE_EXT_OPT"
+test ! -z "$CONFIGURE_FILE_OPT" && echo "  ./configure enable/disable flags: $CONFIGURE_FILE_OPT"
+echo
+
+./configure $CONFIGURE_DEF_OPT $CONFIGURE_EXT_OPT $CONFIGURE_FILE_OPT || {
+        echo "  configure failed"
+        exit 1
+}
+
+echo "Now type 'make' to compile $package."
